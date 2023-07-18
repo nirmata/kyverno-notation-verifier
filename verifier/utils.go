910f483b5a629069eebd45615041f2e398a76dd5
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-07-18 14:01:45 +0530

    feat: update validate request fn to check attests
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- verifier/utils.go
+++ verifier/utils.go
@@ -8,6 +8,35 @@ func validateRequestData(req *RequestData) error {
 		len(req.Images.EphemeralContainers) == 0 {
 		return errors.New("atleast one image must be provided")
 	}
+	for _, att := range req.Attestations {
+		if att.ImageReference == "" {
+			return errors.Errorf("image reference cannot be empty %+v", att)
+		}
 
+		for _, attType := range att.Type {
+			if attType.Name == "" {
+				return errors.Errorf("attestation name cannot be empty %+v: %+v", attType, att)
+			}
+			for _, any := range attType.Conditions.AnyConditions {
+				if any.RawKey == nil {
+					return errors.Errorf("condtion key cannot be empty %+v: %+v: %+v", any, attType, att)
+				}
+
+				if any.RawValue == nil {
+					return errors.Errorf("condtion value cannot be empty %+v: %+v: %+v", any, attType, att)
+				}
+			}
+
+			for _, all := range attType.Conditions.AllConditions {
+				if all.RawKey == nil {
+					return errors.Errorf("condtion key cannot be empty %+v: %+v: %+v", all, attType, att)
+				}
+
+				if all.RawValue == nil {
+					return errors.Errorf("condtion value cannot be empty %+v: %+v: %+v", all, attType, att)
+				}
+			}
+		}
+	}
 	return nil
 }

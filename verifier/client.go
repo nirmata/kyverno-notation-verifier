ac91f2e878d5e146e92b3d0fbba1dbab7120737e
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-08-18 10:50:53 +0530

    feat: add flag for token review
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- verifier/client.go
+++ verifier/client.go
@@ -48,6 +48,7 @@ type verifier struct {
 	maxSignatureAttempts       int
 	maxCacheSize               int64
 	useCache                   bool
+	reviewToken                bool
 	maxCacheTTL                time.Duration
 	debug                      bool
 	stopCh                     chan struct{}
@@ -99,6 +100,12 @@ func WithCacheEnabled(useCache bool) verifierOptsFunc {
 	}
 }
 
+func WithTokenReviewEnabled(enableTokenReview bool) verifierOptsFunc {
+	return func(v *verifier) {
+		v.reviewToken = enableTokenReview
+	}
+}
+
 func WithEnableDebug(debug bool) verifierOptsFunc {
 	return func(v *verifier) {
 		v.debug = debug
@@ -133,42 +140,46 @@ func (v *verifier) HandleCheckImages(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	reqToken := r.Header.Get("Authorization")
-	if len(reqToken) == 0 {
-		http.Error(w, "Authorization header not supplied", http.StatusUnauthorized)
-		return
-	}
-	splitToken := strings.Split(reqToken, "Bearer ")
-	if len(splitToken) == 0 {
-		http.Error(w, "No token provided in the header", http.StatusUnauthorized)
-		return
-	}
-	reqToken = splitToken[1]
+	if v.reviewToken {
+		reqToken := r.Header.Get("Authorization")
+		if len(reqToken) == 0 {
+			http.Error(w, "Authorization header not supplied", http.StatusUnauthorized)
+			return
+		}
+		splitToken := strings.Split(reqToken, "Bearer ")
+		if len(splitToken) == 0 {
+			http.Error(w, "No token provided in the header", http.StatusUnauthorized)
+			return
+		}
+		reqToken = splitToken[1]
 
-	tr := authv1.TokenReview{
-		Spec: authv1.TokenReviewSpec{
-			Token: reqToken,
-		},
-	}
+		tr := authv1.TokenReview{
+			Spec: authv1.TokenReviewSpec{
+				Token: reqToken,
+			},
+		}
 
-	result, err := v.kubeClient.AuthenticationV1().TokenReviews().Create(context.TODO(), &tr, metav1.CreateOptions{})
-	if err != nil {
-		v.logger.Infof("failed to verify auth token %v", err)
-		http.Error(w, err.Error(), http.StatusNotAcceptable)
-		return
-	}
+		result, err := v.kubeClient.AuthenticationV1().TokenReviews().Create(context.TODO(), &tr, metav1.CreateOptions{})
+		if err != nil {
+			v.logger.Infof("failed to verify auth token %v", err)
+			http.Error(w, err.Error(), http.StatusNotAcceptable)
+			return
+		}
 
-	if !result.Status.Authenticated || result.Status.User.Username != "system:serviceaccount:kyverno:kyverno-admission-controller" {
-		v.logger.Infof("Token is not authorized %+v", *result)
-		http.Error(w, "Token is not authorized", http.StatusNotAcceptable)
-		return
+		if !result.Status.Authenticated || result.Status.User.Username != "system:serviceaccount:kyverno:kyverno-admission-controller" {
+			v.logger.Infof("Token is not authorized %+v", *result)
+			http.Error(w, "Token is not authorized", http.StatusNotAcceptable)
+			return
+		}
+
+		v.logger.Infof("Token is authorized %+v", *result)
 	}
 
 	var requestData types.RequestData
 	//err := json.NewDecoder(r.Body).Decode(&requestData)
 	raw, _ := io.ReadAll(r.Body)
 
-	err = json.Unmarshal(raw, &requestData)
+	err := json.Unmarshal(raw, &requestData)
 	if err != nil {
 		v.logger.Infof("failed to decode %s: %v", string(raw), err)
 		http.Error(w, err.Error(), http.StatusNotAcceptable)

6711fc1b5b9583120a04ffe84a8d911ab81dcb6a
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-09-11 19:09:56 +0530

    feat: remove annotation for testing
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- verifier/response.go
+++ verifier/response.go
@@ -1,8 +1,6 @@
 package verifier
 
 import (
-	"encoding/json"
-
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
 	"github.com/kyverno/kyverno/pkg/utils/wildcard"
 	"github.com/nirmata/kyverno-notation-verifier/types"
@@ -91,20 +89,18 @@ func (r *responseStruct) VerificationFailed(msg string) (types.ResponseData, err
 func (r *responseStruct) VerificationSucceeded(msg string) (types.ResponseData, error) {
 	r.responseData.ErrorMessage = msg
 
-	// for testing
-	r.responseData.Results = make([]jsonpatch.Operation, 0)
-	annotationValue, err := json.Marshal(r.ivm.GetAnnotation())
-	if err != nil {
-		return r.responseData, err
-	}
+	// annotationValue, err := json.Marshal(r.ivm.GetAnnotation())
+	// if err != nil {
+	// 	return r.responseData, err
+	// }
 
-	annotatationPatch := jsonpatch.Operation{
-		Operation: r.ivm.GetOperation(),
-		Path:      r.ivm.GetAnnotationKeyForJSONPatch(),
-		Value:     string(annotationValue),
-	}
+	// annotatationPatch := jsonpatch.Operation{
+	// 	Operation: r.ivm.GetOperation(),
+	// 	Path:      r.ivm.GetAnnotationKeyForJSONPatch(),
+	// 	Value:     string(annotationValue),
+	// }
 
-	r.responseData.Results = append(r.responseData.Results, annotatationPatch)
+	// r.responseData.Results = append(r.responseData.Results, annotatationPatch)
 
 	r.log.Infof("Sending response result=%+v", r.responseData.Results)
 	return r.responseData, nil
